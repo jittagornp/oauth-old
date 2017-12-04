fe9dc3c3ce98f21d8047a5232686f2fd6f685183
jittagornp <jittagornp@gmail.com>
2017-12-04 11:40:20 +0700

    add LoginService_loadUserByUsernameTest

---
+++ oauth2/src/test/java/com/pamarin/oauth2/service/LoginService_loadUserByUsernameTest.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2017 Pamarin.com
+ */
+package com.pamarin.oauth2.service;
+
+import com.pamarin.oauth2.LoginServiceImpl;
+import com.pamarin.oauth2.domain.User;
+import com.pamarin.oauth2.repository.UserRepo;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.mockito.Mockito.when;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+/**
+ * @author jittagornp &lt;http://jittagornp.me&gt; create : 2017/12/04
+ */
+public class LoginService_loadUserByUsernameTest {
+
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
+    @InjectMocks
+    private LoginServiceImpl loginService;
+
+    @Mock
+    private UserRepo userRepo;
+
+    @Before
+    public void before() {
+        MockitoAnnotations.initMocks(this);
+        when(userRepo.findByUsername("test"))
+                .thenReturn(null);
+        when(userRepo.findByUsername("root"))
+                .thenReturn(new User());
+    }
+
+    @Test
+    public void shouldBeErrorRequireId_whenInputIsNull() {
+
+        exception.expect(UsernameNotFoundException.class);
+        exception.expectMessage("Require id.");
+
+        String input = null;
+        UserDetails output = loginService.loadUserByUsername(input);
+
+    }
+    
+    @Test
+    public void shouldBeErrorUserNotFound_whenInputIsTest() {
+
+        exception.expect(UsernameNotFoundException.class);
+        exception.expectMessage("User not found.");
+
+        String input = "test";
+        UserDetails output = loginService.loadUserByUsername(input);
+
+    }
+    
+    @Test
+    public void shouldBeOk() {
+        String input = "root";
+        UserDetails output = loginService.loadUserByUsername(input);
+        assertThat(output).isNotNull();
+    }
+
+}
