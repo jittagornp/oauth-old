edca82ff8adde8c89b9267927ef00195b7de3644
jitta <jitta@DESKTOP-8QIAGPC>
2019-01-17 00:29:53 +0700

    custom redis session prefix

---
+++ oauth2/src/main/java/com/pamarin/oauth2/config/RedisApplicationListener.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2017 Pamarin.com
+ */
+package com.pamarin.oauth2.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.security.web.session.HttpSessionCreatedEvent;
+import org.springframework.security.web.session.HttpSessionDestroyedEvent;
+import org.springframework.session.data.redis.RedisOperationsSessionRepository;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+/**
+ *
+ * @author jitta
+ */
+@EnableRedisHttpSession
+public class RedisApplicationListener implements ApplicationListener<ApplicationEvent> {
+    
+    private static final Logger LOG = LoggerFactory.getLogger(RedisApplicationListener.class);
+
+    @Value("${spring.session.timeout}")
+    private Integer maxInactiveIntervalInSeconds;
+
+    @Autowired
+    private RedisOperationsSessionRepository redisOperation;
+
+    @Override
+    public void onApplicationEvent(ApplicationEvent event) {
+        
+        if (event instanceof ContextRefreshedEvent) {
+            LOG.debug("ContextRefreshedEvent..., redisOperation.setDefaultMaxInactiveInterval({})", maxInactiveIntervalInSeconds);
+            redisOperation.setDefaultMaxInactiveInterval(maxInactiveIntervalInSeconds);
+            return;
+        }
+        
+        if(event instanceof HttpSessionCreatedEvent){
+            HttpSessionCreatedEvent ev = (HttpSessionCreatedEvent)event;
+            LOG.debug("session id {} was created...", ev.getSession().getId());
+            return;
+        }
+        
+        if(event instanceof HttpSessionDestroyedEvent){
+            HttpSessionDestroyedEvent ev = (HttpSessionDestroyedEvent)event;
+            LOG.debug("session id {} expires...", ev.getId());
+        }
+        
+    }
+
+}
--- oauth2/src/main/java/com/pamarin/oauth2/config/RedisConf.java
+++ oauth2/src/main/java/com/pamarin/oauth2/config/RedisConf.java
@@ -3,15 +3,17 @@
  */
 package com.pamarin.oauth2.config;
 
-import javax.validation.constraints.NotNull;
-import org.hibernate.validator.constraints.NotBlank;
+import java.lang.reflect.Field;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.session.data.redis.RedisFlushMode;
+import org.springframework.session.data.redis.RedisOperationsSessionRepository;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.util.ReflectionUtils;
+
 /**
  * @author jittagornp &lt;http://jittagornp.me&gt; create : 2017/11/12
  */
@@ -19,22 +21,29 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 @EnableRedisHttpSession
 @Profile("!test") //inactive for test profile
 public class RedisConf {
-    
-//    @NotBlank
-//    @Value("${spring.redis.host}")
-//    private String host; 
-//    
-//    @NotNull
-//    @Value("${spring.redis.port}")
-//    private Integer port;
 
-//    @Bean
-//    public RedisConnectionFactory jedisConnectionFactory() {
-//        JedisConnectionFactory jedisConFactory = new JedisConnectionFactory();
-//        //jedisConFactory.setHostName(host);
-//        //jedisConFactory.setPort(port);
-//        jedisConFactory.se
-//        return jedisConFactory;
-//    }
+    @Value("${spring.session.timeout}")
+    private Integer maxInactiveIntervalInSeconds;
+
+    @Value("${spring.session.redis.namespace}")
+    private String namespace;
+
+    @Value("${spring.session.redis.flush-mode}")
+    private String flushMode;
+
+    @Bean
+    public RedisOperationsSessionRepository sessionRepository(RedisConnectionFactory factory) {
+        RedisOperationsSessionRepository sessionRepository = new RedisOperationsSessionRepository(factory);
+
+        sessionRepository.cleanupExpiredSessions();
+        
+        Field keyPrefixField = ReflectionUtils.findField(RedisOperationsSessionRepository.class, "keyPrefix");
+        keyPrefixField.setAccessible(true);
+        ReflectionUtils.setField(keyPrefixField, sessionRepository, namespace + ":");
+        
+        sessionRepository.setDefaultMaxInactiveInterval(maxInactiveIntervalInSeconds);
+        sessionRepository.setRedisFlushMode("on-save".equals(flushMode) ? RedisFlushMode.ON_SAVE : RedisFlushMode.IMMEDIATE);
+        return sessionRepository;
+    }
 
 }
