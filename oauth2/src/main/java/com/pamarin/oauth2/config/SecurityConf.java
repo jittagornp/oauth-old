4249be617b6457819d7859a1bca270781a55dd07
jitta <jitta@DESKTOP-8QIAGPC>
2019-01-28 22:29:53 +0700

    ignore static resource

--- oauth2/src/main/java/com/pamarin/oauth2/config/SecurityConf.java
+++ oauth2/src/main/java/com/pamarin/oauth2/config/SecurityConf.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -47,6 +48,14 @@ public class SecurityConf extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers("/assets/**", "/favicon.ico");
+    }
+
+    
+    
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
