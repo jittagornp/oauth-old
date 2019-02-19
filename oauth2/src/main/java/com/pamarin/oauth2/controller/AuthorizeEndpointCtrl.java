a6c1eb3c9e9978291632608647257eda7be0f8c4
jitta <jitta@DESKTOP-8QIAGPC>
2019-02-19 16:28:56 +0700

    add CrossOrigin to /authorize

--- oauth2/src/main/java/com/pamarin/oauth2/controller/AuthorizeEndpointCtrl.java
+++ oauth2/src/main/java/com/pamarin/oauth2/controller/AuthorizeEndpointCtrl.java
@@ -19,6 +19,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,6 +31,7 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @Controller
 @RequestMapping("/authorize")
+@CrossOrigin("*")
 public class AuthorizeEndpointCtrl {
 
     private static final Logger LOG = LoggerFactory.getLogger(AuthorizeEndpointCtrl.class);
