0f70083d69c8db0ccbc262f057bc680f070e2ac8
jitta <jitta@DESKTOP-8QIAGPC>
2019-03-09 13:42:51 +0700

    add LoginSession_getSessionIdTest and LoginSession_logoutTest

---
+++ commons/src/test/java/com/pamarin/commons/security/LoginSession_getSessionIdTest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2017 Pamarin.com
+ */
+package com.pamarin.commons.security;
+
+import com.pamarin.commons.provider.HttpSessionProvider;
+import java.util.UUID;
+import javax.servlet.http.HttpSession;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.springframework.test.util.ReflectionTestUtils;
+
+/**
+ *
+ * @author jitta
+ */
+public class LoginSession_getSessionIdTest {
+
+    private LoginSession loginSession;
+
+    private HttpSessionProvider httpSessionProvider;
+
+    private HttpSession session;
+
+    @Before
+    public void before() {
+        session = mock(HttpSession.class);
+        httpSessionProvider = mock(HttpSessionProvider.class);
+        loginSession = new DefaultLoginSession();
+        ReflectionTestUtils.setField(
+                loginSession,
+                "httpSessionProvider",
+                httpSessionProvider
+        );
+    }
+
+    @Test
+    public void shouldBeNull_whenSessionIsNull() {
+        when(httpSessionProvider.provide()).thenReturn(null);
+
+        String output = loginSession.getSessionId();
+        String expected = null;
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    public void shouldBeOk() {
+        String sessionId = UUID.randomUUID().toString();
+        when(session.getId()).thenReturn(sessionId);
+        when(httpSessionProvider.provide()).thenReturn(session);
+
+        String output = loginSession.getSessionId();
+        String expected = sessionId;
+        assertThat(output).isEqualTo(expected);
+    }
+}
---
+++ commons/src/test/java/com/pamarin/commons/security/LoginSession_logoutTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2017 Pamarin.com
+ */
+package com.pamarin.commons.security;
+
+import com.pamarin.commons.provider.HttpSessionProvider;
+import javax.servlet.http.HttpSession;
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.springframework.test.util.ReflectionTestUtils;
+
+/**
+ *
+ * @author jitta
+ */
+public class LoginSession_logoutTest {
+
+    private LoginSession loginSession;
+
+    private HttpSessionProvider httpSessionProvider;
+
+    private HttpSession session;
+
+    @Before
+    public void before() {
+        session = mock(HttpSession.class);
+        httpSessionProvider = mock(HttpSessionProvider.class);
+        loginSession = new DefaultLoginSession();
+        ReflectionTestUtils.setField(
+                loginSession,
+                "httpSessionProvider",
+                httpSessionProvider
+        );
+    }
+
+    @Test
+    public void shouldBeCallInvalidate_whenSessionIsNotNull() {
+        when(httpSessionProvider.provide()).thenReturn(session);
+
+        loginSession.logout();
+
+        verify(session, times(1)).invalidate();
+        verify(session).setMaxInactiveInterval(0);
+    }
+}
