8210a449aa3c4a7c9305e75fb6b4e0ed5a753fd5
jitta <jitta@DESKTOP-8QIAGPC>
2019-03-09 13:10:52 +0700

    enabled test case LoginSession_getUserDetailsTest

--- commons/src/test/java/com/pamarin/commons/security/LoginSession_wasCreatedTest.java
+++ commons/src/test/java/com/pamarin/commons/security/LoginSession_wasCreatedTest.java
@@ -1,43 +1,42 @@
-///*
-// * Copyright 2017 Pamarin.com
-// */
-//package com.pamarin.commons.security;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//import org.junit.Before;
-//import org.junit.Rule;
-//import org.junit.Test;
-//import org.junit.rules.ExpectedException;
-//
-///**
-// * @author jittagornp &lt;http://jittagornp.me&gt; create : 2017/11/21
-// */
-//public class LoginSession_wasCreatedTest {
-//
-//    @Rule
-//    public final ExpectedException exception = ExpectedException.none();
-//
-//    private LoginSession loginSession;
-//
-//    @Before
-//    public void before() {
-//        loginSession = new DefaultLoginSession();
-//    }
-//
-////    @Test
-////    public void shouldBeFalse_whenNotLogin() {
-////        boolean output = loginSession.wasCreated();
-////        boolean expected = false;
-////        assertThat(output).isEqualTo(expected);
-////    }
-////TODO
-////
-////    @Test
-////    public void shouldBeTrue_whenLogedIn() {
-////        loginSession.create(UserDetailsStub.get());
-////        boolean output = loginSession.wasCreated();
-////        boolean expected = true;
-////        assertThat(output).isEqualTo(expected);
-////    }
-//
-//}
+/*
+ * Copyright 2017 Pamarin.com
+ */
+package com.pamarin.commons.security;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+/**
+ * @author jittagornp &lt;http://jittagornp.me&gt; create : 2017/11/21
+ */
+public class LoginSession_wasCreatedTest {
+
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
+    private LoginSession loginSession;
+
+    @Before
+    public void before() {
+        loginSession = new DefaultLoginSession();
+    }
+
+    @Test
+    public void shouldBeFalse_whenNotLogin() {
+        boolean output = loginSession.wasCreated();
+        boolean expected = false;
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    public void shouldBeTrue_whenLogedIn() {
+        loginSession.create(UserDetailsStub.get());
+        boolean output = loginSession.wasCreated();
+        boolean expected = true;
+        assertThat(output).isEqualTo(expected);
+    }
+
+}
