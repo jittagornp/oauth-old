c369fdb0a19b92c25d9743fcd95132f0aabafcec
jitta <jitta@DESKTOP-8QIAGPC>
2019-03-27 15:24:49 +0700

    Save Oauth2AccessToken to Mongodb

--- oauth2/src/main/java/com/pamarin/oauth2/config/RedisConf.java
+++ oauth2/src/main/java/com/pamarin/oauth2/config/RedisConf.java
@@ -3,10 +3,6 @@
  */
 package com.pamarin.oauth2.config;
 
-import com.pamarin.oauth2.OAuth2AccessTokenRepoImpl;
-import com.pamarin.oauth2.RedisOAuth2AccessTokenRepo;
-import com.pamarin.oauth2.RedisOAuth2AuthorizationCodeRepo;
-import com.pamarin.oauth2.RedisOAuth2RefreshTokenRepo;
 import com.pamarin.oauth2.RedisSessionRepositoryImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,9 +14,6 @@ import org.springframework.session.SessionRepository;
 import org.springframework.session.config.annotation.web.http.SpringHttpSessionConfiguration;
 import org.springframework.session.data.redis.RedisFlushMode;
 import com.pamarin.oauth2.repository.DatabaseSessionRepo;
-import com.pamarin.oauth2.repository.OAuth2AccessTokenRepo;
-import com.pamarin.oauth2.repository.OAuth2AuthorizationCodeRepo;
-import com.pamarin.oauth2.repository.OAuth2RefreshTokenRepo;
 
 /**
  * @author jittagornp &lt;http://jittagornp.me&gt; create : 2017/11/12
@@ -40,13 +33,7 @@ public class RedisConf extends SpringHttpSessionConfiguration {
 
     @Autowired
     private DatabaseSessionRepo databaseSessionRepo;
-
-    @Value("${spring.session.access-token.timeout}")
-    private Integer accessTokenTimeout;
-
-    @Value("${spring.session.refresh-token.timeout}")
-    private Integer refreshTokenTimeout;
-
+    
     @Bean
     public SessionRepository sessionRepository(RedisConnectionFactory factory) {
         RedisSessionRepositoryImpl sessionRepository = new RedisSessionRepositoryImpl(factory);
@@ -55,25 +42,5 @@ public class RedisConf extends SpringHttpSessionConfiguration {
         sessionRepository.setRedisFlushMode("on-save".equals(flushMode) ? RedisFlushMode.ON_SAVE : RedisFlushMode.IMMEDIATE);
         sessionRepository.setDatabaseSessionRepository(databaseSessionRepo);
         return sessionRepository;
-    }
-
-    @Bean
-    public OAuth2AuthorizationCodeRepo newOAuth2AuthorizationCodeRepo() {
-        return new RedisOAuth2AuthorizationCodeRepo(1);
-    }
-
-    @Bean
-    public RedisOAuth2AccessTokenRepo newRedisOAuth2AccessTokenRepo() {
-        return new RedisOAuth2AccessTokenRepo(accessTokenTimeout / 60);
-    }
-
-    @Bean
-    public OAuth2RefreshTokenRepo newOAuth2RefreshTokenRepo() {
-        return new RedisOAuth2RefreshTokenRepo(refreshTokenTimeout / 60);
-    }
-
-    @Bean
-    public OAuth2AccessTokenRepo newOAuth2AccessTokenRepo() {
-        return new OAuth2AccessTokenRepoImpl();
     }
 }
