70d3f085d3f0afb9d0c4f987940f88a1617d549b
jitta <jitta@DESKTOP-8QIAGPC>
2019-03-27 10:35:07 +0700

    add test PrincipalNameResolverTest

---
+++ commons/src/test/java/com/pamarin/commons/resolver/PrincipalNameResolverTest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2017-2019 Pamarin.com
+ */
+package com.pamarin.commons.resolver;
+
+import com.pamarin.commons.security.DefaultUserDetails;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextImpl;
+import org.springframework.session.Session;
+
+/**
+ *
+ * @author jitta
+ */
+public class PrincipalNameResolverTest {
+
+    private PrincipalNameResolver resolver;
+
+    @Before
+    public void before() {
+        resolver = new DefaultPrincipalNameResolver();
+    }
+
+    @Test
+    public void shouldBeNull_whenUserNotLogin() {
+        Session input = mock(Session.class);
+        String output = resolver.resolve(input);
+        String expected = null;
+
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    public void shouldBeJittagornp_whenUserLoggedIn() {
+        Session input = mock(Session.class);
+        SecurityContext context = new SecurityContextImpl();
+        context.setAuthentication(new UsernamePasswordAuthenticationToken(
+                DefaultUserDetails.builder()
+                        .username("jittagornp")
+                        .build(),
+                null
+        ));
+        when(input.getAttribute("SPRING_SECURITY_CONTEXT")).thenReturn(context);
+
+        String output = resolver.resolve(input);
+        String expected = "jittagornp";
+
+        assertThat(output).isEqualTo(expected);
+    }
+}
