615c9caeebb2af6e2dc0d199a860d3dcfdd8780e
jitta <jitta@DESKTOP-8QIAGPC>
2019-03-29 23:30:13 +0700

    refactoring code

--- account/src/main/java/com/pamarin/account/config/SecurityConf.java
+++ account/src/main/java/com/pamarin/account/config/SecurityConf.java
@@ -5,14 +5,26 @@
  */
 package com.pamarin.account.config;
 
+import com.pamarin.oauth2.client.sdk.AuthenticationEntryPointAdapter;
+import com.pamarin.oauth2.client.sdk.OAuth2SessionRetriever;
+import java.io.IOException;
 import java.util.UUID;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 /**
  *
@@ -23,14 +35,11 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConf extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private AuthenticationEntryPoint authenticationEntryPoint;
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .exceptionHandling()
-                .authenticationEntryPoint(authenticationEntryPoint)
+                .authenticationEntryPoint(newAuthenticationEntryPoint())
                 .and()
                 .headers().frameOptions().disable()
                 .and()
@@ -51,4 +60,52 @@ public class SecurityConf extends WebSecurityConfigurerAdapter {
                 .logoutUrl("logout-" + UUID.randomUUID().toString()); //not publish logout url to outside
     }
 
+    @Bean
+    public AuthenticationEntryPoint newAuthenticationEntryPoint() {
+        return new AuthenticationEntryPointAdapter() {
+
+            @Value("${oauth2.authorization-server.hostUrl}")
+            private String authorizationServerHostUrl;
+
+            @Value("${server.hostUrl}")
+            private String hostUrl;
+
+            @Override
+            protected String getAuthorizationServerHostUrl() {
+                return authorizationServerHostUrl;
+            }
+
+            @Override
+            protected String getHostUrl() {
+                return hostUrl;
+            }
+
+            @Override
+            protected String getClientId() {
+                return "b98e21b4-ce2a-11e7-abc4-cec278b6b50a";
+            }
+
+            @Override
+            protected String getScope() {
+                return "user:public_profile";
+            }
+
+        };
+    }
+
+    @Bean("sessionFilter")
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public OncePerRequestFilter newSessionFilter() {
+        return new OncePerRequestFilter() {
+
+            @Autowired
+            private OAuth2SessionRetriever oauth2SessionRetriever;
+
+            @Override
+            protected void doFilterInternal(HttpServletRequest httpReq, HttpServletResponse httpResp, FilterChain chain) throws ServletException, IOException {
+                oauth2SessionRetriever.retrieve(httpReq, httpResp);
+                chain.doFilter(httpReq, httpResp);
+            }
+        };
+    }
 }
