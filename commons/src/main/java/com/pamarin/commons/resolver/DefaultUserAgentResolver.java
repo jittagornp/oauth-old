91fc433f0ff37525152c352003a2ea83d2598c35
jitta <jitta@DESKTOP-8QIAGPC>
2019-04-05 20:49:53 +0700

    refactoring code DefaultUserAgentResolver

--- commons/src/main/java/com/pamarin/commons/resolver/DefaultUserAgentResolver.java
+++ commons/src/main/java/com/pamarin/commons/resolver/DefaultUserAgentResolver.java
@@ -3,6 +3,14 @@
  */
 package com.pamarin.commons.resolver;
 
+import eu.bitwalker.useragentutils.Browser;
+import eu.bitwalker.useragentutils.BrowserType;
+import eu.bitwalker.useragentutils.DeviceType;
+import eu.bitwalker.useragentutils.Manufacturer;
+import eu.bitwalker.useragentutils.OperatingSystem;
+import eu.bitwalker.useragentutils.RenderingEngine;
+import static eu.bitwalker.useragentutils.UserAgent.parseUserAgentString;
+import eu.bitwalker.useragentutils.Version;
 import java.util.Optional;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Component;
@@ -22,13 +30,12 @@ public class DefaultUserAgentResolver implements UserAgentResolver {
             return null;
         }
 
-        eu.bitwalker.useragentutils.UserAgent agent = eu.bitwalker.useragentutils.UserAgent.parseUserAgentString(header);
         UserAgent userAgent = UserAgent.builder().build();
-        
-        extractBrowser(agent, userAgent);
-        extractOperatingSystem(agent, userAgent);
-        extractVerion(agent, userAgent);
-
+        $(parseUserAgentString(header)).ifPresent(agent -> {
+            copyBrowser(agent.getBrowser(), userAgent);
+            copyOperatingSystem(agent.getOperatingSystem(), userAgent);
+            copyVerion(agent.getBrowserVersion(), userAgent);
+        });
         return userAgent;
     }
 
@@ -36,59 +43,80 @@ public class DefaultUserAgentResolver implements UserAgentResolver {
         return Optional.ofNullable(value);
     }
 
-    private void extractBrowser(eu.bitwalker.useragentutils.UserAgent agent, UserAgent userAgent) {
-        $(agent.getBrowser()).ifPresent(browser -> {
-            userAgent.setBrowserKey(browser.name());
-            userAgent.setBrowserName(browser.getName());
-
-            $(browser.getRenderingEngine()).ifPresent(renderingEngine -> {
-                userAgent.setBrowserRenderingEngine(renderingEngine.name());
-            });
-
-            $(browser.getManufacturer()).ifPresent(manufacturer -> {
-                userAgent.setBrowserManufacturerKey(manufacturer.name());
-                userAgent.setBrowserManufacturerName(manufacturer.getName());
-            });
-
-            $(browser.getGroup()).ifPresent(group -> {
-                userAgent.setBrowserGroupKey(group.name());
-                userAgent.setBrowserGroupName(group.getName());
-            });
-
-            $(browser.getBrowserType()).ifPresent(type -> {
-                userAgent.setBrowserTypeKey(type.name());
-                userAgent.setBrowserTypeName(type.getName());
-            });
+    private void copyBrowser(Browser browser, UserAgent userAgent) {
+        $(browser).ifPresent(bws -> {
+            userAgent.setBrowserKey(bws.name());
+            userAgent.setBrowserName(bws.getName());
+            copyRenderingEngine(bws.getRenderingEngine(), userAgent);
+            copyBrowerManufacturer(bws.getManufacturer(), userAgent);
+            copyBrowserGroup(bws.getGroup(), userAgent);
+            copyBrowserType(bws.getBrowserType(), userAgent);
         });
     }
 
-    private void extractOperatingSystem(eu.bitwalker.useragentutils.UserAgent agent, UserAgent userAgent) {
-        $(agent.getOperatingSystem()).ifPresent(os -> {
+    private void copyRenderingEngine(RenderingEngine renderingEngine, UserAgent userAgent) {
+        $(renderingEngine).ifPresent(engine -> {
+            userAgent.setBrowserRenderingEngine(engine.name());
+        });
+    }
+
+    private void copyBrowerManufacturer(Manufacturer manufacturer, UserAgent userAgent) {
+        $(manufacturer).ifPresent(man -> {
+            userAgent.setBrowserManufacturerKey(man.name());
+            userAgent.setBrowserManufacturerName(man.getName());
+        });
+    }
+
+    private void copyBrowserGroup(Browser browser, UserAgent userAgent) {
+        $(browser).ifPresent(group -> {
+            userAgent.setBrowserGroupKey(group.name());
+            userAgent.setBrowserGroupName(group.getName());
+        });
+    }
+
+    private void copyBrowserType(BrowserType browserType, UserAgent userAgent) {
+        $(browserType).ifPresent(type -> {
+            userAgent.setBrowserTypeKey(type.name());
+            userAgent.setBrowserTypeName(type.getName());
+        });
+    }
+
+    private void copyOperatingSystem(OperatingSystem operatingSystem, UserAgent userAgent) {
+        $(operatingSystem).ifPresent(os -> {
             userAgent.setOsKey(os.name());
             userAgent.setOsName(os.getName());
-
-            $(os.getDeviceType()).ifPresent(type -> {
-                userAgent.setDeviceTypeKey(type.name());
-                userAgent.setDeviceTypeName(type.getName());
-            });
-
-            $(os.getManufacturer()).ifPresent(manufacturer -> {
-                userAgent.setOsManufacturerKey(manufacturer.name());
-                userAgent.setOsManufacturerName(manufacturer.getName());
-            });
-
-            $(os.getGroup()).ifPresent(group -> {
-                userAgent.setOsGroupKey(group.name());
-                userAgent.setOsGroupName(group.getName());
-            });
+            copyDeviceType(os.getDeviceType(), userAgent);
+            copyOsManufacturer(os.getManufacturer(), userAgent);
+            copyOsGroup(os.getGroup(), userAgent);
         });
     }
 
-    private void extractVerion(eu.bitwalker.useragentutils.UserAgent agent, UserAgent userAgent) {
-        $(agent.getBrowserVersion()).ifPresent(version -> {
-            userAgent.setAgentVersion(version.getVersion());
-            userAgent.setAgentMajorVersion(version.getMajorVersion());
-            userAgent.setAgentMinorVersion(version.getMinorVersion());
+    private void copyDeviceType(DeviceType deviceType, UserAgent userAgent) {
+        $(deviceType).ifPresent(type -> {
+            userAgent.setDeviceTypeKey(type.name());
+            userAgent.setDeviceTypeName(type.getName());
+        });
+    }
+
+    private void copyOsManufacturer(Manufacturer osManufacturer, UserAgent userAgent) {
+        $(osManufacturer).ifPresent(manufacturer -> {
+            userAgent.setOsManufacturerKey(manufacturer.name());
+            userAgent.setOsManufacturerName(manufacturer.getName());
+        });
+    }
+
+    private void copyOsGroup(OperatingSystem osGroup, UserAgent userAgent) {
+        $(osGroup).ifPresent(group -> {
+            userAgent.setOsGroupKey(group.name());
+            userAgent.setOsGroupName(group.getName());
+        });
+    }
+
+    private void copyVerion(Version version, UserAgent userAgent) {
+        $(version).ifPresent(vs -> {
+            userAgent.setAgentVersion(vs.getVersion());
+            userAgent.setAgentMajorVersion(vs.getMajorVersion());
+            userAgent.setAgentMinorVersion(vs.getMinorVersion());
         });
     }
 
