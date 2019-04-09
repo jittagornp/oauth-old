5aecd709243ccc29ed575934710075ad1083585f
jitta <jitta@DESKTOP-8QIAGPC>
2019-04-09 21:19:53 +0700

    add Security Configuration

---
+++ oauth2-client/src/main/java/com/pamarin/oauth2/client/SecurityConf.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2017-2019 Pamarin.com
+ */
+package com.pamarin.oauth2.client;
+
+import java.util.UUID;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ *
+ * @author jitta
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConf extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.headers().frameOptions().disable()
+                .and()
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers(
+                        "/public/**",
+                        "/static/**",
+                        "/assets/**",
+                        "/favicon.ico",
+                        "/",
+                        "/code"
+                )
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .logout()
+                .logoutUrl("logout-" + UUID.randomUUID().toString()); //not publish logout url to outside
+    }
+}
