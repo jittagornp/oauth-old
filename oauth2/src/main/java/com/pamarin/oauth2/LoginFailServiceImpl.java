09b235c11319e55c8026395ab7b57ac0c13fe8d6
jitta <jitta@DESKTOP-8QIAGPC>
2019-05-05 17:39:16 +0700

    add login fail service

--- oauth2/src/main/java/com/pamarin/oauth2/LoginFailServiceImpl.java
+++ oauth2/src/main/java/com/pamarin/oauth2/LoginFailServiceImpl.java
@@ -4,9 +4,13 @@
 package com.pamarin.oauth2;
 
 import com.pamarin.commons.generator.IdGenerator;
+import com.pamarin.commons.provider.HttpServletRequestProvider;
+import com.pamarin.commons.resolver.HttpClientIPAddressResolver;
 import com.pamarin.oauth2.collection.LoginFailHistory;
+import com.pamarin.oauth2.resolver.UserAgentTokenIdResolver;
 import com.pamarin.oauth2.service.LoginFailService;
 import static java.time.LocalDateTime.now;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.stereotype.Service;
@@ -22,27 +26,43 @@ public class LoginFailServiceImpl implements LoginFailService {
 
     private final MongoOperations mongoOperations;
 
+    private final HttpServletRequestProvider httpServletRequestProvider;
+
+    private final HttpClientIPAddressResolver httpClientIPAddressResolver;
+
+    private final UserAgentTokenIdResolver userAgentTokenIdResolver;
+
     @Autowired
-    public LoginFailServiceImpl(IdGenerator idGenerator, MongoOperations mongoOperations) {
+    public LoginFailServiceImpl(
+            IdGenerator idGenerator,
+            MongoOperations mongoOperations,
+            HttpServletRequestProvider httpServletRequestProvider,
+            HttpClientIPAddressResolver httpClientIPAddressResolver,
+            UserAgentTokenIdResolver userAgentTokenIdResolver
+    ) {
         this.idGenerator = idGenerator;
         this.mongoOperations = mongoOperations;
+        this.httpServletRequestProvider = httpServletRequestProvider;
+        this.httpClientIPAddressResolver = httpClientIPAddressResolver;
+        this.userAgentTokenIdResolver = userAgentTokenIdResolver;
     }
 
     @Override
     public void collect(String username) {
+        HttpServletRequest httpReq = httpServletRequestProvider.provide();
         LoginFailHistory history = LoginFailHistory.builder()
                 .id(idGenerator.generate())
                 .username(username)
                 .failDate(now())
-                .agentId(username)
-                .ipAddress(username)
+                .agentId(userAgentTokenIdResolver.resolve(httpReq))
+                .ipAddress(httpClientIPAddressResolver.resolve(httpReq))
                 .build();
         mongoOperations.save(history, LoginFailHistory.COLLECTION_NAME);
     }
 
     @Override
     public void verify(String username) {
-    
+
     }
 
 }
