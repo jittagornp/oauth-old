4b7c8d7cab0d7576bca6e04701e2d30a5ea82ca7
jitta <jitta@DESKTOP-8QIAGPC>
2019-05-06 10:34:42 +0700

    cache agentId in http request

--- oauth2/src/main/java/com/pamarin/oauth2/resolver/DefaultUserAgentTokenIdResolver.java
+++ oauth2/src/main/java/com/pamarin/oauth2/resolver/DefaultUserAgentTokenIdResolver.java
@@ -7,6 +7,7 @@ import com.pamarin.commons.resolver.DefaultHttpCookieResolver;
 import com.pamarin.commons.resolver.HttpCookieResolver;
 import com.pamarin.commons.util.Base64Utils;
 import javax.servlet.http.HttpServletRequest;
+import org.springframework.util.Assert;
 import static org.springframework.util.StringUtils.hasText;
 
 /**
@@ -14,6 +15,8 @@ import static org.springframework.util.StringUtils.hasText;
  * @author jitta
  */
 public class DefaultUserAgentTokenIdResolver implements UserAgentTokenIdResolver {
+
+    private static final String CACHED_KEY = UserAgentTokenIdResolver.class.getName() + ".AGENT_ID";
 
     private final HttpCookieResolver httpCookieResolver;
 
@@ -23,10 +26,22 @@ public class DefaultUserAgentTokenIdResolver implements UserAgentTokenIdResolver
 
     @Override
     public String resolve(HttpServletRequest httpReq) {
+        Assert.notNull(httpReq, "require httpReq.");
+
+        String cached = (String) httpReq.getAttribute(CACHED_KEY);
+        if (hasText(cached)) {
+            return cached;
+        }
+
         String cookieValue = httpCookieResolver.resolve(httpReq);
         if (!hasText(cookieValue)) {
             return null;
         }
-        return Base64Utils.decode(cookieValue);
+        return cached(Base64Utils.decode(cookieValue), httpReq);
+    }
+
+    private String cached(String agentId, HttpServletRequest httpReq) {
+        httpReq.setAttribute(CACHED_KEY, agentId);
+        return agentId;
     }
 }
