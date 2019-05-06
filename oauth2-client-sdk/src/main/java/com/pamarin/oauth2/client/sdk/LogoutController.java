1e2a862713a35e07b795b4d7650f3bc7f0465ae5
jitta <jitta@DESKTOP-8QIAGPC>
2019-05-06 12:53:31 +0700

    logout by redirection if logout by backend service fail

--- oauth2-client-sdk/src/main/java/com/pamarin/oauth2/client/sdk/LogoutController.java
+++ oauth2-client-sdk/src/main/java/com/pamarin/oauth2/client/sdk/LogoutController.java
@@ -4,13 +4,16 @@
 package com.pamarin.oauth2.client.sdk;
 
 import com.pamarin.commons.provider.HostUrlProvider;
+import com.pamarin.commons.util.QuerystringBuilder;
 import java.io.IOException;
 import static java.lang.String.format;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.HttpClientErrorException;
 
 /**
  *
@@ -36,16 +39,37 @@ public class LogoutController {
         this.accessTokenResolver = accessTokenResolver;
     }
 
-    @GetMapping("/logout")
-    public void logout(HttpServletRequest httpReq, HttpServletResponse httpResp) throws IOException {
+    private String getLogoutUrl() {
+        return format("%s/logout?%s",
+                clientOperations.getAuthorizationServerHostUrl(),
+                new QuerystringBuilder()
+                        .addParameter("client_id", clientOperations.getClientId())
+                        .addParameter("redirect_uri", hostUrlProvider.provide())
+                        .build()
+        );
+    }
+
+    private void logoutFromBackendService(HttpServletRequest httpReq) {
         clientOperations.post(
                 format("%s/logout", clientOperations.getAuthorizationServerHostUrl()),
                 null,
                 String.class,
                 accessTokenResolver.resolve(httpReq)
         );
+    }
 
-        httpResp.sendRedirect(hostUrlProvider.provide());
+    @GetMapping("/logout")
+    public void logout(HttpServletRequest httpReq, HttpServletResponse httpResp) throws IOException {
+        try {
+            logoutFromBackendService(httpReq);
+            httpResp.sendRedirect(hostUrlProvider.provide());
+        } catch (HttpClientErrorException ex) {
+            if (ex.getStatusCode() != HttpStatus.UNAUTHORIZED) {
+                throw ex;
+            }
+
+            httpResp.sendRedirect(getLogoutUrl());
+        }
     }
 
 }
